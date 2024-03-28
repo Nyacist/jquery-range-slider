ea90a0f6a9ba9acf07c44567ada782e4ad064513
Olga Kashirina <old.deus.73@gmail.com>
2024-03-28 17:03:32 +0300

    new types, thumbs array => thumbs obj

--- src/types/types.ts
+++ src/types/types.ts
@@ -27,5 +27,30 @@ export const SIDE = {
 } as const
 export type SIDE = keyof typeof SIDE
 
-export type Thumbs = [IThumb] | [IThumb, IThumb]
 
+export type THUMBS_ARRAY = {
+  [key: string]: IThumb
+}
+//   {
+//   [key in LOCATION]?: IThumb
+// }
+
+export type THUMBS_VIEW = {
+  [key: string]: JQuery<HTMLElement>
+}
+//   {
+//   [key in LOCATION]?: JQuery<HTMLElement>
+// }
+
+
+export type SLIDER_SETTINGS = {
+  direction: DIRECTION;
+  qtThumbs: QT_THUMBS;
+  gap: number;
+}
+
+type Concrete<Type> = {
+  [Property in keyof Type]+?: Type[Property];
+};
+
+export type SLIDER_PROPS_OPTIONS = Concrete<SLIDER_SETTINGS>
